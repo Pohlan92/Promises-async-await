89e80b57a67970c0bb68a02dc69be8790633c759
Pohlonenko <Pohlonenko@mail.ru>
2023-05-23 14:05:24 +0300

    Доработка

--- src/js/modules/GameSavingLoader.js
+++ src/js/modules/GameSavingLoader.js
@@ -1,17 +1,13 @@
-import read from './reader.js';
-import json from './parser.js';
+import read from "./reader.js";
+import json from "./parser.js";
 
 export default class GameSavingLoader {
   static load() {
     return read()
-      .then(result => json(result), 
-            error => new Error(error)
-      )
-      .then(data => JSON.parse(data),
-            error => new Error(error) 
-      )
-
-      .catch(error => console.error(error)) 
-   
+      .then((result) => json(result))
+      .then(
+        (data) => JSON.parse(data),
+        (error) => new Error(error)
+      );
   }
 }
